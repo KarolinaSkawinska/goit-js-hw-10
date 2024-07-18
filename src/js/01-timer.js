2ec0f9ec4e4ae26459946822378134540df6db1d
KarolinaSkawinska <karolinaa1607@gmail.com>
2024-07-18 15:46:14 +0200

    correction

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,6 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const startButton = document.querySelector('[data-start]');
 const daysSpan = document.querySelector('[data-days]');
 const hoursSpan = document.querySelector('[data-hours]');
